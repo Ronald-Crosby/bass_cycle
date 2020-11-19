67391553d17d29b465c53d4a399fdad7423e017b
Olly Robinson <49555108+Ronald-Crosby@users.noreply.github.com>
2020-11-19 14:52:09 +0000

    2021 marquee

--- _assets/js/marquee.js
+++ _assets/js/marquee.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-	function createVerticalMarquee(parentEl, textContent) {
+	function createVerticalMarquee(parentEl, textContent, repeatNum) {
 		const textSpan = document.createElement('span')
 		textSpan.appendChild(textContent)
 
-		const marquee = new Array(1000).fill(textSpan)
+		const marquee = new Array(Number(repeatNum)).fill(textSpan)
 		marquee.forEach(span => {
 			parentEl.append(span.cloneNode(true))
 		})
 	}
 
-	const marquees = document.querySelectorAll('.js-vertical-marquee')
+	const marquees = document.querySelectorAll('.js-marquee')
 	marquees.forEach(marquee => {
 		const textContent = marquee.firstElementChild
-		createVerticalMarquee(marquee, textContent)
+		const repeatNum = marquee.dataset.repeatNum
+		createVerticalMarquee(marquee, textContent, repeatNum)
 	})
 })
 
